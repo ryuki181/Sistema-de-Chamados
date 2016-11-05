1862f8bd9641ba5a6496cefaf5de5c64b42ec85e
Dani <Dani@Lugli>
2016-11-04 23:47:56 -0200

    Criação do teste de alteração de chamado.

---
+++ test/controle/ControleChamadosTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+import entidade.Chamado;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dani
+ */
+public class ControleChamadosTest {
+
+    public ControleChamadosTest() {
+    }
+
+    @Test
+    public void testAlterarChamado() {
+        Chamado chamadoTeste = new Chamado(1, null, null, 1, null, null, null, null, null, null);
+        ControleChamados cc = new ControleChamados();
+        cc.alterarChamado(chamadoTeste, "Ativo", "Causa Teste", "Solução Teste");
+
+        assertEquals(chamadoTeste.getCausaProblema(), "Causa Teste");
+    }
+
+}
